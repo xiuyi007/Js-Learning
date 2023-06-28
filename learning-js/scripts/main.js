0ef4544677907b7147fc99d94b4772e54d048c2f
togethf <1720425326@qq.com>
2023-06-28 17:36:19 +0800

    initial commit

---
+++ learning-js/scripts/main.js
@@ -0,0 +1,10 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!"
+
+document.querySelector("html").addEventListener("click", () => {
+    alert("Ouch! Stop poking me !")
+})
+function multiply(num1, num2)
+{
+    return num1 * num2;
+}
